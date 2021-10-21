90155f5a02a5ef7021883b2bf798219f64451121
Yusepp <joseplcam@gmail.com>
2021-10-21 03:30:11 +0200

    Improving Plot

--- utils/data.py
+++ utils/data.py
@@ -8,8 +8,20 @@ import numpy as np
 def read_image(path):
    return imread(path), path
 
-def load_data(folder: str, extension: str) -> Tuple[List[np.ndarray], List[str]]:
-   data = Parallel(n_jobs=2)(delayed(read_image)(folder+image) for image in os.listdir(folder) if extension in image)
+def load_data(folder: str, extension: str, desc: str):
+   files = [folder+image for image in os.listdir(folder) if extension in image]
+   data = Parallel(n_jobs=2)(delayed(read_image)(file) for file in tqdm(files, desc = desc))
    print('{} read: {} images'.format(folder,len(data)))
    images, images_names = list(zip(*data))
    return images, images_names
+
+
+def image_to_windows(image: np.ndarray, n_rows: int, n_cols: int):
+    x, y = image.shape[0]/n_rows, image.shape[1]/n_cols
+    x, y = int(x), int(y)
+    windows = []
+    for i in range(n_rows):
+       for j in range(n_cols):
+           windows.append(image[i*x:(i+1)*x, j*y:(j+1)*y])
+    
+    return windows
